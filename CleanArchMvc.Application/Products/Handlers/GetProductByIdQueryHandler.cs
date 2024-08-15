8b2fd02c982f8559042f3d6e8ac36eace92046d0
Cristiane Fernandes <cris.pessin@gmail.com>
2024-08-15 19:30:19 -0300

    feat: CQRS - implementado os Handlers para Commands e Queries.

---
+++ CleanArchMvc.Application/Products/Handlers/GetProductByIdQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchMvc.Application.Products.Queries;
+using CleanArchMvc.Domain.Entites;
+using CleanArchMvc.Domain.Interfaces;
+using MediatR;
+
+namespace CleanArchMvc.Application.Products.Handlers
+{
+    public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, Product>
+    {
+        IProductRepository _productRepository;
+
+        public GetProductByIdQueryHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<Product> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _productRepository.GetByIdAsync(request.Id);
+        }
+    }
+}
---
+++ CleanArchMvc.Application/Products/Handlers/ProductRemoveCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using CleanArchMvc.Application.Products.Commands;
+using CleanArchMvc.Domain.Entites;
+using CleanArchMvc.Domain.Interfaces;
+using MediatR;
+
+namespace CleanArchMvc.Application.Products.Handlers
+{
+    public class ProductRemoveCommandHandler : IRequestHandler<ProductRemoveCommand, Product>
+    {
+        IProductRepository _productRepository;
+
+        public ProductRemoveCommandHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ??
+                throw new ArgumentNullException(nameof(productRepository));
+        }
+
+        public async Task<Product> Handle(ProductRemoveCommand request, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetByIdAsync(request.Id);
+
+            if (product == null)
+            {
+                throw new ApplicationException($"Entity could be not found.");
+            }
+            else
+            {
+                var result = await _productRepository.RemoveAsync(product);
+                return await _productRepository.UpdateAsync(product);
+            }
+        }
+    }
+}
