7b46cebc277cd8cf1d3c081aa2540cc23d7f2357
Cristiane Fernandes <cris.pessin@gmail.com>
2024-08-19 15:59:52 -0300

    feat: criado interfaces da camada Domain.

---
+++ CleanArchMvc.Domain/Account/IAuthenticate.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanArchMvc.Domain.Account
+{
+    public interface IAuthenticate
+    {
+        Task<bool> Authenticate(string username, string password);
+        Task<bool> RegisterUser(string username, string password);
+        Task<bool> Logout();
+    }
+}
